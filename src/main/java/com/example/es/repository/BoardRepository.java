3a9294beb043ed3f8fd642bfb48db1d348eb8bf2
wtkim <rladnjsxo123@naver.com>
2019-04-02 08:59:09 +0900

    edit Board repo tests

--- src/main/java/com/example/es/repository/BoardRepository.java
+++ src/main/java/com/example/es/repository/BoardRepository.java
@@ -2,6 +2,8 @@ package com.example.es.repository;
 
 
 import com.example.es.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -12,7 +14,14 @@ import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board,Long> {
 
-    public List<Board> findBoardByTitle(String title);
+    List<Board> findBoardByTitle(String title);
 
-    public Collection<Board> findBoardByWriter(String Writer);
+    Collection<Board> findBoardByWriter(String Writer);
+
+    List<Board> findByIdGreaterThanOrderByIdDesc(Long Id, Pageable paging);
+
+
+    Page<Board> findByIdGreaterThan( Long Id, Pageable paging);
+
+
 }
--- src/test/java/com/example/es/repository/BoardRepositoryTests.java
+++ src/test/java/com/example/es/repository/BoardRepositoryTests.java
@@ -6,10 +6,15 @@ import com.example.es.domain.Board;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 @RunWith(SpringRunner.class)
@@ -82,4 +87,47 @@ public class BoardRepositoryTests {
 
 
     }
+
+    @Test
+    public void testIdOrderByPagin(){
+        //
+        Pageable paging = new PageRequest(0,10);
+
+        Collection<Board> results = boardRepository.findByIdGreaterThanOrderByIdDesc(0L,paging);
+
+        results.forEach(System.out::println);
+
+    }
+
+
+    @Test
+    public void testIdPagingSort(){
+
+        Pageable paging = new PageRequest(0,10, Sort.Direction.ASC, "Id");
+
+
+        // Page<T> 를 이용하면 spring MVC와 연동할 때 편리하다 .
+        Page<Board> results = boardRepository.findByIdGreaterThan(0L,paging);
+
+        System.out.println("Page Size : "+ results.getSize() );
+        System.out.println("total pages : "+ results.getTotalPages() );
+        System.out.println("total count : "+ results.getTotalElements() );
+        System.out.println("next : "+ results.nextPageable() );
+
+        List<Board> list = results.getContent();
+
+        list.forEach(System.out::println);
+
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
